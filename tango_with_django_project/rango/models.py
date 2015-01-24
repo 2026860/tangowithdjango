5cd5aba763aeea370175c445085c7781bfa59e78
Konstantin <2026860i@student.gla.ac.uk>
2015-01-24 20:11:06 +0000

    Chapter 6.7

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.db.models import Model
 # Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length = 128, unique = True)
+
+	def __unicode__(self):
+		return self.name
+
+class Page(models.Model):
+	#CharField stores character data
+	#URLField stores URL resources
+	#IntegerField stores integers
+	#ForeignKey,a field type that allows us to create a one-to-many relationship
+
+	category = models.ForeignKey(Category)
+	title = models.CharField(max_length = 128) 
+	url = models.URLField() 
+	views = models.IntegerField(default = 0)
+
+	def __unicode__(self):
+		return self.title
