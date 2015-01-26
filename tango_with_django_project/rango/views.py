159163975638f1c0ae3c1ab594896615f8ce0816
Konstantin <2026860i@student.gla.ac.uk>
2015-01-26 15:26:02 +0000

    Chapter 7 - completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
-# Create your views here.
+#Models Imports
+from rango.models import Page
+from rango.models import Category
 
+
+# Create your views here.
 def index(request):
     
-    context_dict = {'boldmessage': "I am bold font from the context"}
-    
+    category_list = Category.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list}
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     return render(request, 'rango/about.html')
+
+def category(request, category_name_slug):
+    
+    context_dict = {}
+    
+    try:
+        category = Category.objects.get(slug = category_name_slug)
+        context_dict['category name'] = category.name
+        
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        
+        context_dict['category'] = category
+    
+    except Category.DoesNotExist:
+        pass
+    
+    return render(request, 'rango/category.html', context_dict)
