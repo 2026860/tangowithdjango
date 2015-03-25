5eda7598a1db95cb869f11457b97d0e7055fceef
Konstantin <2026860i@student.gla.ac.uk>
2015-03-25 21:03:09 +0000

    ''

--- tango_with_django_project/rango/tests.py
+++ tango_with_django_project/rango/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from django.core.urlresolvers import reverse
 # Create your tests here.
+
+class IndexViewTests(TestCase):
+
+    def test_index_view_with_no_categories(self):
+        """
+        If no questions exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "There are no categories present.")
+        self.assertQuerysetEqual(response.context['categories'], [])
